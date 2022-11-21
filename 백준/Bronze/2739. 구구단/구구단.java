3bfec8415359f09b53df9347ba030b8ccfbef8c1
스터디문 <117332905+MinhaLim@users.noreply.github.com>
2022-11-21 18:08:30 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18536 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        for(int i = 1;i<10;i++){
+            System.out.println(a + " * " + i + " = " + a*i);
+        }
+    }
+}
