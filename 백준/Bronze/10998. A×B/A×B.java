3c758bff9275831a5e09ec4efed6bfa14c94dfe4
스터디문 <117332905+MinhaLim@users.noreply.github.com>
2022-11-21 18:12:00 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        int A,B;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+        
+        System.out.printf("%d",A*B);
+    }
+}
