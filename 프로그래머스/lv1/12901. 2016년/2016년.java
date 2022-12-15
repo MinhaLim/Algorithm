4928864afb16060fb3da83bbffc10d15c5d97160
스터디문 <117332905+MinhaLim@users.noreply.github.com>
2022-12-15 23:23:15 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,14 +1,12 @@
 class Solution {
-    static int[] days = {0,31,29,31,30,31,30,31,31,30,31,30,31};
-    static String[] weeks = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
-
     public String solution(int a, int b) {
+        int[] days = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+        String[] weeks = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
         String answer = "";
         int day=b;
         if(a==1){
             day = day%7-1;
             if(day==-1)day=6;
-            //System.out.println(day);
             answer = weeks[day];
         }else{
             for(int i=1; i<a; i++){
