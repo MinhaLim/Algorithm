d16681f26c21b815f85e249acbbed73acd79ea20
스터디문 <117332905+MinhaLim@users.noreply.github.com>
2022-12-18 15:57:29 +0900

    [level 1] Title: 소수 만들기, Time: 0.45 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] nums) {
+        int answer=0;
+		int len=nums.length;
+		
+		Arrays.sort(nums);
+		for(int i=0;i<len-2;i++) {
+			for(int j=i+1;j<len-1;j++) {
+				for(int k=j+1;k<len;k++) {
+					int num = nums[i]+nums[j]+nums[k];
+					for(int a=2;a<=num;a++) {
+						if(num==a) {
+							answer++;
+						}else if(num%a==0) {
+							break;
+						}
+					}
+				}
+			}
+		}
+        return answer;
+    }
+}
